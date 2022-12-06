c2e09325838d465710ebc55cdaeb0d206005acc1
Ashley Coventry <ashleycoventry@ucsb.edu>
2022-12-06 00:35:48 -0800

    added code converting actual age to actual age range & tried to write conevrsion of ideal partner age to ideak partner age difference but getting errors

--- Human Data Analysis/PR Bisexuality Analysis Script 11.19.R
+++ Human Data Analysis/PR Bisexuality Analysis Script 11.19.R
@@ -22,13 +22,39 @@ data<-read.csv(file.choose())
   #how do i standardize these?
   #what does it mean to standardize these lol
   #zscore, scale()
+prefs <- scale(data[,178:197])
+
+###converting preferred age to preferred age difference
+  #130 = m_lt_age, 142 = f_lt_age, 154 = f_st_age, 166 = m_st_age
+  #20 or younger = 1, 21-26 = 2, 27-32 = 3, 33-38 = 4, 39-44 = 5, 
+  #45-50 = 6, 51-56 = 7, 57-62 = 8, 63-68 = 9, 69-74 = 10
+
+##converting participant age to same scale as ideal
+data$ageLik <- ifelse(data$age >= 69, 10,
+                      ifelse(data$age >= 63, 9,
+                             ifelse(data$age >= 57, 8,
+                                    ifelse(data$age >= 51, 7, 
+                                           ifelse(data$age >= 45, 6, 
+                                                  ifelse(data$age >= 39, 5, 
+                                                         ifelse(data$age >= 33, 4, 
+                                                                ifelse(data$age >= 27, 3, 
+                                                                       ifelse(data$age >= 21, 2, 1)))))))))
+                      
 
 
-##age conversion (ideal age -> ideal age diff)
-  #data$AGELIK <- ifelse(data$age) >= 75, 10, 
-    #ifelse......
-  #data$partnerage - data$agelik
+##converting ideal partner age to ideal partner age difference
 
+#male ideal LT
+data$m_lt_age_lik <- data$m_lt_age - data$agelik
+
+#male ideal ST
+data$m_st_age_lik <- data$m_st_age - data$agelik
+
+#female ideal LT
+data$f_lt_age_lik <- data$f_lt_age - data$agelik
+
+#female ideal ST
+data$f_st_age_lik <- data$f_st_age - data$agelik
 
 
 ### LT prefs multilevel model ###
